dda22088d96e78fa89280f9f8dff0da17397d2d2
Sajjad Haider <sani723@hotmail.com>
2017-09-26 12:39:04 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <div>
+    <h1>Welcome To React!</h1>
+  </div>
+  , document.getElementById('root'));
+registerServiceWorker();
