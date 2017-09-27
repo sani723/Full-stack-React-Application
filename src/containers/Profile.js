7f4c0116a6e01ebc334910e822352d203aa4ce71
Sajjad Haider <sani723@hotmail.com>
2017-09-27 16:52:41 +0400

    Profile added

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,11 +1,62 @@
 import React, {Component} from 'react';
+import {Container, Name, GameListHeader, GameList, GameRecord, Column, ColumnLabels} from '../styled/Profile';
 
 class Profile extends Component {
-  render() {
+
+static defaultProps = {
+  user: {
+    email: 'USER_EMAIL',
+    games: [
+      {
+        winner: true,
+        createdAt: '12/25/2017',
+        id: '0001'
+      },
+      {
+        winner: true,
+        createdAt: '12/26/2017',
+        id: '0002'
+      },
+      {
+        winner: true,
+        createdAt: '12/27/2017',
+        id: '0003'
+      },
+    ]
+  }
+};
+
+get records() {
+  return this.props.user.games.map( (game,index)=>{
     return (
-      <div>
-        <h2>Profile!</h2>
-      </div>
+      <GameRecord key={index} index={index}>
+        <Column>{(game.winner) ? "Won" : "Didn't win"}</Column>
+        <Column>"Robot"</Column>
+        <Column>"No"</Column>
+        <Column>{game.createdAt}</Column>
+      </GameRecord>
+    );
+  });
+}
+
+  render() {
+    let {email} = this.props.user;
+    return (
+      <Container>
+        <Name>
+          {email}
+        </Name>
+        <GameList>
+          <GameListHeader>My Games</GameListHeader>
+            <ColumnLabels>
+              <Column>Outcome</Column>
+              <Column>Guess</Column>
+              <Column>Guessed Correctly</Column>
+              <Column>Date</Column>
+            </ColumnLabels>
+            {this.records}
+        </GameList>
+      </Container>
     );
   }
 }
